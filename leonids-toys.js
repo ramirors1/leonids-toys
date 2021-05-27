4dc70f6c1ca87da6c8145eff273cce7e9ec63044
ramirors1 <ramirezr@protonmail.com>
2021-05-27 11:47:29 -0500

    first attempt

---
+++ leonids-toys.js
@@ -0,0 +1,25 @@
+const truck = {
+    id: 1,
+    name: "Truck",
+    maker: "Toy World",
+    price: 18,
+    location: "isle 3",
+    weight: 3.2
+
+}
+const football = {
+    id: 2,
+    name: "Football",
+    maker: "Sports World",
+    price: 21,
+    locaiton: "Isle 5",
+    weight: 2.1
+}
+const skateboard = {
+    id: 3,
+    name: "Skateboard",
+    maker: "Street Rollerz",
+    price: 32,
+    location: "Isle 7",
+    weight: 4.9
+}
