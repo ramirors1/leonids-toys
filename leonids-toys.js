2b19fc8b6f9eda09f7a79908c0121f14d8be24c2
ramirors1 <ramirezr@protonmail.com>
2021-05-27 15:19:10 -0500

    latest version

--- leonids-toys.js
+++ leonids-toys.js
@@ -1,25 +1,51 @@
-const truck = {
+const toys = [ 
+{
     id: 1,
     name: "Truck",
     maker: "Toy World",
     price: 18,
     location: "isle 3",
     weight: 3.2
+},
 
-}
-const football = {
+{
     id: 2,
     name: "Football",
     maker: "Sports World",
     price: 21,
     locaiton: "Isle 5",
     weight: 2.1
-}
-const skateboard = {
+},
+
+{
     id: 3,
     name: "Skateboard",
     maker: "Street Rollerz",
     price: 32,
     location: "Isle 7",
     weight: 4.9
+}
+]
+const frisbee = {
+    id: 4,
+    name: "Frisbee",
+    maker: "Cloney",
+    price: 4,
+    location: "Isle 9",
+    weight: .7
+} 
+
+const bike = {
+    id: 5,
+    name: "Bike",
+    maker: "2Wheels",
+    price: 250,
+    locaiton: "Isle 12",
+    weight: 42
+}
+toys.push(frisbee)
+toys.push(bike)
+
+for (const toy of toys) {
+console.log(`The ${toy.name} cost ${toy.price} dollars.`)
 }
